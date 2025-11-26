3258e1b4881f432749fc7d71291edc8c16d53778
Aymeric Cassard <aymeric.cassard@laposte.net>
2025-11-26 22:25:45 +0100

    Set correct prod base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://aymericcassard.github.io/',
   // Only use base path in production (GitHub Pages)
-  base: process.env.NODE_ENV === 'production' ? '/astro-theme-terminal' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/AymericCassard.github.io' : '/',
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
